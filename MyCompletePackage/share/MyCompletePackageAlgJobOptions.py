22a0d771a6850ace7c24ed1d0447661af3d76571
Aviv Ruben Cukierman <acukierm@lxplus062.cern.ch>
2016-03-15 21:05:20 +0100

    Moved RecExCommon/RecExCommon_topOptions.py to end, added myAlg.py

--- MyCompletePackage/share/MyCompletePackageAlgJobOptions.py
+++ MyCompletePackage/share/MyCompletePackageAlgJobOptions.py
@@ -1,22 +1,12 @@
 #Skeleton joboption for a simple analysis job
 
 theApp.EvtMax=10                                         #says how many events to run over. Set to -1 for all events
-#include ("RecExCommon/RecExCommon_topOptions.py")
 
 import AthenaPoolCnvSvc.ReadAthenaPool                   #sets up reading of POOL files (e.g. xAODs)
 svcMgr.EventSelector.InputCollections=["/afs/cern.ch/user/a/acukierm/Datasets/AOD.05771185._000079.pool.root.1"]   #insert your list of input files here
 #ToolSvc += CfgMgr.CVFTool("MyCVFTool",InputContainer="CaloCalTopoClusters",OutputContainer="CVFClusters",doSpread=False)
 
-try:
-  from eflowRec.eflowRecConf import eflowTrackCaloExtensionTool
-  TrackCaloExtensionTool=eflowTrackCaloExtensionTool()
-except:
-  mlog.error("could not import eflowRec.eflowTrackCaloExtensionTool")
-  print traceback.format_exc()
-CfgMgr.MyCompletePackageAlg().TrackExtrapolatorTool = TrackCaloExtensionTool
-
-algseq = CfgMgr.AthSequencer("AthAlgSeq")                #gets the main AthSequencer
-algseq += CfgMgr.MyCompletePackageAlg()                                 #adds an instance of your alg to it
-
 include("AthAnalysisBaseComps/SuppressLogging.py")       #Optional include to suppress as much athena output as possible
 
+UserAlgs = ["myAlg.py"]
+include ("RecExCommon/RecExCommon_topOptions.py")
