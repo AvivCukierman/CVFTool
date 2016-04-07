a7093623f0ce9bc9e746db588b7a8493b3baa2ce
Aviv Ruben Cukierman <acukierm@lxplus059.cern.ch>
2016-04-08 00:31:05 +0200

    Additional run script to add to RecExCommon

---
+++ run.py
@@ -0,0 +1,5 @@
+from AthenaCommon.AthenaCommonFlags import athenaCommonFlags
+#athenaCommonFlags.FilesInput=["/data/hodgkinson/scratchFiles/mc15_13TeV.361022.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ2W.merge.AOD.e3668_s2576_s2132_r6765_r6282/AOD.05771189._000200.pool.root.1"]
+athenaCommonFlags.FilesInput=["/afs/cern.ch/user/a/acukierm/Datasets/AOD.05771185._000079.pool.root.1"]
+athenaCommonFlags.EvtMax=10
+include("RecExCommon/RecExCommon_topOptions.py")
